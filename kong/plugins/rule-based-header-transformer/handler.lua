48db476fedc50bd89762e6c4a9ac17d8c914f8da
bencso <gabor.bencsik13@gmail.com>
2019-05-07 17:16:43 +0200

    CDPIP-1356: Remove boilerplate code
    Co-Authored-By: npospischek <npospischek@users.noreply.github.com>

--- kong/plugins/rule-based-header-transformer/handler.lua
+++ kong/plugins/rule-based-header-transformer/handler.lua
@@ -2,7 +2,7 @@ local BasePlugin = require "kong.plugins.base_plugin"
 
 local RuleBasedHeaderTransformerHandler = BasePlugin:extend()
 
-RuleBasedHeaderTransformerHandler.PRIORITY = 2000
+RuleBasedHeaderTransformerHandler.PRIORITY = 904
 
 function RuleBasedHeaderTransformerHandler:new()
   RuleBasedHeaderTransformerHandler.super.new(self, "rule-based-header-transformer")
@@ -10,18 +10,6 @@ end
 
 function RuleBasedHeaderTransformerHandler:access(conf)
   RuleBasedHeaderTransformerHandler.super.access(self)
-
-  if conf.say_hello then
-    kong.log.debug('Hey!')
-
-    kong.service.request.set_header('X-Upstream-Header', 'Hey Upstream!')
-    kong.response.set_header('X-Downstream-Header', 'Hey Downstream!')
-  else
-    kong.log.debug('Bye!')
-
-    kong.service.request.set_header('X-Upstream-Header', 'Bye Upstream!')
-    kong.response.set_header('X-Downstream-Header', 'Bye Downstream!')
-  end
 
 end
 
